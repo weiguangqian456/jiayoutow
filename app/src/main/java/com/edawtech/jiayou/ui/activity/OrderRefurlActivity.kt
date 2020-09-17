3b28807f68cb05a3cfd9ad93b98d0dae5fe73535
weiguangqian456 <1156017221@qq.com>
2020-09-17 11:01:45 +0800

    搜索

---
+++ app/src/main/java/com/edawtech/jiayou/ui/activity/OrderRefurlActivity.kt
@@ -0,0 +1,12 @@
+package com.edawtech.jiayou.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.edawtech.jiayou.R
+
+class OrderRefurlActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_refurl)
+    }
+}
