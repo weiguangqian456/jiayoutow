425e86fed4acb1dccd95e44c69ba54f177690ce1
Lrin <1023141631@qq.com>
2020-09-23 16:11:13 +0800

    用户资料逻辑补充，成长金部分

--- app/src/main/java/com/edawtech/jiayou/ui/fragment/MineFragment.java
+++ app/src/main/java/com/edawtech/jiayou/ui/fragment/MineFragment.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
